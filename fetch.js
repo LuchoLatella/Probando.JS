8fe5c8e072563310a48b89ae18909f29bff60ae7
LuchoLatella <lucho_latella@hotmail.com>
2026-04-16 18:52:49 -0300

    iniciando el repo

---
+++ fetch.js
@@ -0,0 +1,37 @@
+
+// fetch('https://fakestoreapi.com/products/1')
+//             .then(res=>res.json())            
+//             .then(json=>console.log(json))
+
+            // Gererar en la web de fake api la conexion  https://fakestoreapi.com/
+
+//         {
+//   id: 1,
+//   title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+//   price: 109.95,
+//   description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+//   category: "men's clothing",
+//   image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+//   rating: { rate: 3.9, count: 120 }
+// }                                                                    resultado de la consulta a la API
+
+
+// MEJORAR EL CÓDIGO DE FETCH
+
+const url = 'https://fakestoreapi.com/products';    //dejando constante la URL
+
+fetch(url)
+    .then((response)=>{
+        if(!response.ok){                               //viendo en caso de error
+            console.log('Error');
+        }
+        return response.json();                         // en caso ok, retornar el json
+    })
+    .then((datos) => {
+        console.log(datos);                             // genero los mismos datos pero con un mejor manejo de errores
+    
+    })
+
+
+    // .then(res => res.json())
+    // .then(json=>console.log(json))
